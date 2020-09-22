51acd99869bebfb582a1b7a4980359d34254ed06
Dorel P <dorel.pureca@gmail.com>
2020-09-22 20:08:56 +0200

    Week 3 - working with arrays

---
+++ w3/Arrays/Project_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] userNumbers = new int[3];
+
+            userNumbers[0] = 3;
+
+            for (int i = 0; i < userNumbers.Length; i++)
+            {
+                Console.WriteLine("Input number "+(i+1)+":");
+                //myFirstArray[i] = Convert.ToInt32(Console.ReadLine());
+                userNumbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Clear();
+            Console.WriteLine("You provided the following numbers");
+
+            for (int i = 0; i < userNumbers.Length; i++)
+            {
+                Console.Write($"{userNumbers[i]}, ");
+
+            }
+
+            Console.WriteLine();
+            foreach (var number in userNumbers)
+            {
+                Console.Write($"{number}, ");
+            }
+
+            Console.WriteLine();
+            int counter = 0;
+            while (counter < userNumbers.Length)
+            {
+                Console.Write($"{userNumbers[counter]}, ");
+                counter++;
+            }
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
---
+++ w3/Arrays/Project_2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+
+namespace Project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print array of random numbers
+            int[] numbers = CreateArray();
+            PrintArray(numbers);
+            Console.WriteLine();
+
+            // Max from array
+            Console.WriteLine($"Max number is: {GetMaxFromArray(numbers)}");
+            Console.WriteLine();
+
+            // Number of occurences of an element
+            Console.WriteLine($"{numbers[0]} appears {GetOccurences(numbers)}");
+            Console.WriteLine();
+
+            // Sun of even numbers
+            Console.WriteLine($"Total sum of even numbers is {GetSumOfEvenNumbers(numbers)}");
+            Console.WriteLine();
+
+            Console.ReadLine();
+
+        }
+
+        static int[] CreateArray()
+        {
+            int[] numbers = new int[5];
+            Random rnd = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(0, 40);
+            }
+
+            return numbers;
+        }
+
+        static void PrintArray(int[] arrayToPrint)
+        {
+            foreach (var number in arrayToPrint)
+            {
+                Console.Write($"{number}, ");
+            }
+        }
+
+        static int GetMaxFromArray(int[] numbers)
+        {
+            int max = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                    max = numbers[i];
+            }
+            return max;
+        }
+
+        static int GetOccurences(int[] numbers)
+        {
+            int counter = 0;
+            int reference = numbers[0];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i]==reference)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        static int GetSumOfEvenNumbers(int[] numbers)
+        {
+            int sum = 0;
+
+            foreach (var number in numbers)
+            {
+                if (number % 2 == 0)
+                    sum = sum + number;
+            }
+            return sum;
+        }
+
+    }
+}
